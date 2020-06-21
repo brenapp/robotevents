ed7fa4634180dbbd390c3802a42476e65dbef1ac
Brendan McGuire <brendan@bren.app>
2020-06-21 03:17:40 -0400

    Get RobotEvents cookie

---
+++ src/authentication.ts
@@ -0,0 +1,30 @@
+/**
+ * Manages RobotEvents API authentication
+ * In the future this may support EP accounts, but for now we're only supporting the read only API
+ */
+
+import * as parse from "set-cookie-parser";
+
+export let COOKIE = "";
+
+import fetch from "node-fetch";
+
+export default async function authenticate() {
+  const response = await fetch("https://www.robotevents.com");
+
+  if (!response.ok) {
+    return Promise.reject(
+      new Error("Could not get CSRF token or Authentication Cookie")
+    );
+  }
+
+  const cookie = parse(response.headers.raw()["set-cookie"]);
+
+  // Invalidate the cookie after re_session expires
+  const re_session = cookie.find((c) => c.name === "re_session");
+  setTimeout(() => {
+    COOKIE = "";
+  }, re_session.maxAge);
+
+  return (COOKIE = cookie.map((c) => `${c.name}=${c.value}`).join("; "));
+}
