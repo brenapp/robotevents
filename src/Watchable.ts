4787c30896ea6881e943cfd7c86203da6ee1891f
Brendan McGuire <brendan@bren.app>
2020-06-22 15:51:08 -0400

    Watchable Classes

---
+++ src/Watchable.ts
@@ -0,0 +1,77 @@
+/**
+ * Makes a class Watchable
+ *
+ * For example, allows you to moniter events for updates
+ *
+ */
+
+import { EventEmitter } from "events";
+
+export type Get<T> = () => Promise<T> | T;
+
+interface WatchableEvents<T, I extends keyof T = keyof T> {
+  update: (key: I, current: T[I], old: T[I]) => void;
+}
+
+interface Watchable<T> {
+  on<U extends keyof WatchableEvents<T>>(
+    event: U,
+    listener: WatchableEvents<T>[U]
+  ): this;
+  once<U extends keyof WatchableEvents<T>>(
+    event: U,
+    listener: WatchableEvents<T>[U]
+  ): this;
+  off<U extends keyof WatchableEvents<T>>(
+    event: U,
+    listener: WatchableEvents<T>[U]
+  ): this;
+}
+
+abstract class Watchable<T> extends EventEmitter {
+  check: Get<T>;
+  watchable: boolean;
+  frequency: number = 30 * 1000;
+  interval: NodeJS.Timeout | null = null;
+  polling = false;
+
+  constructor(check: Get<T>) {
+    super();
+    this.watchable = !!check;
+    this.check = check;
+  }
+
+  watch(frequency?: number) {
+    if (frequency) {
+      this.frequency = frequency;
+    }
+
+    this.polling = true;
+
+    this.interval = setInterval(async () => {
+      if (!this.check) return;
+
+      const state = await this.check();
+
+      for (const [key, value] of Object.entries(state)) {
+        // @ts-ignore
+        const current = JSON.stringify(this[key]);
+        const updated = JSON.stringify(value);
+
+        if (current !== updated) {
+          this.emit("update", key, updated, current);
+        }
+      }
+    }, this.frequency);
+  }
+
+  unwatch() {
+    this.polling = false;
+
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+}
+
+export default Watchable;
