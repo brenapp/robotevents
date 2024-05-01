ef6150b7cf44eacb0b57f1bec58e3d3ca09b8d4d
GuoLin <mr.guo.lin@gmail.com>
2024-05-01 21:46:50 +0800

    add cookie support

--- src/util/authentication.ts
+++ src/util/authentication.ts
@@ -3,6 +3,7 @@
  */
 
 export let BEARER = "";
+export let COOKIE = "";
 
 /**
  * Sets the Bearer Token to provide to robotevents. This is useful if you have
@@ -15,10 +16,22 @@ export function setBearer(bearer: string) {
 }
 
 /**
+ * Sets the Cookie to provide to robotevents.
+ * because access API by bearer will not get the limit info in Response.header.
+ * but When access API by cookie, they will give the limit info back.
+ * So We can set the cookie instead of the official Bearer.
+ *
+ * @param cookie
+ */
+export function setCookie(cookie: string) {
+  return (COOKIE = cookie);
+}
+
+/**
  * Checks if the user agent has been authenticated correctly. If this function
  * returns true you may not be ok to make requests, as your session cookie may
  * have been revoked early, however it is generally a good indicator
  */
 export function ok() {
-  return BEARER !== "";
+  return BEARER !== "" || COOKIE != "";
 }
