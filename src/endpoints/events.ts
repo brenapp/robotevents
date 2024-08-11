5105fa4f2580fe95f56424027cb6c5e734dd81b9
Brendan McGuire <brendan@bren.app>
2024-08-10 21:26:01 -0500

    Ensure that all types return FetcherResponse

--- src/endpoints/events.ts
+++ src/endpoints/events.ts
@@ -1,27 +1,50 @@
 import { Events } from "../generated/robotevents.js";
-import { EndpointOptions } from "../types.js";
+import { EndpointOptions, FetcherResponse } from "../types.js";
 import { Event } from "../wrappers/Event.js";
 
 export function createEventsEndpoint(api: EndpointOptions) {
   return {
-    async search(options: Events.EventGetEvents.RequestQuery) {
+    async search(
+      options: Events.EventGetEvents.RequestQuery
+    ): Promise<FetcherResponse<Event[]>> {
       const response = await api.paginatedFetch<
         Events.EventGetEvents.ResponseBody,
         Events.EventGetEvents.RequestQuery
       >("/events", options);
 
-      return response.success
-        ? (response.data?.map((data) => new Event(data, api)) ?? [])
-        : [];
+      if (!response.success) {
+        return response;
+      }
+
+      const data = response.data?.map((data) => new Event(data, api)) ?? [];
+      return { success: true, data };
     },
 
-    async get(id: number) {
+    async getBySKU(sku: string): Promise<FetcherResponse<Event | null>> {
+      const response = await api.paginatedFetch<
+        Events.EventGetEvents.ResponseBody,
+        Events.EventGetEvents.RequestQuery
+      >("/events", { "sku[]": [sku] });
+
+      if (!response.success) {
+        return response;
+      }
+
+      const event = response.data?.[0];
+      return { success: true, data: event ? new Event(event, api) : null };
+    },
+
+    async get(id: number): Promise<FetcherResponse<Event | null>> {
       const response = await api.fetch<
         Events.EventGetEvent.ResponseBody,
         Events.EventGetEvent.RequestQuery
       >(`/events/${id}`);
 
-      return response.success ? new Event(response.data, api) : null;
+      if (!response.success) {
+        return response;
+      }
+
+      return { success: true, data: new Event(response.data, api) };
     },
   };
 }
--- src/endpoints/teams.ts
+++ src/endpoints/teams.ts
@@ -1,27 +1,56 @@
 import { Teams } from "../generated/robotevents.js";
-import { EndpointOptions, TeamData } from "../types.js";
+import {
+  EndpointOptions,
+  Fetcher,
+  FetcherResponse,
+  ProgramCode,
+} from "../types.js";
 import { Team } from "../wrappers/Team.js";
 
 export function createTeamsEndpoint(api: EndpointOptions) {
   return {
-    async search(options: Teams.TeamGetTeams.RequestQuery) {
+    async search(
+      options: Teams.TeamGetTeams.RequestQuery
+    ): Promise<FetcherResponse<Team[]>> {
       const response = await api.paginatedFetch<
         Teams.TeamGetTeams.ResponseBody,
         Teams.TeamGetTeams.RequestQuery
       >("/teams", options);
 
-      return response.success
-        ? (response.data?.map((data) => new Team(data, api)) ?? [])
-        : [];
+      if (!response.success) {
+        return response;
+      }
+
+      const data = response.data?.map((data) => new Team(data, api)) ?? [];
+      return { success: true, data };
     },
 
-    async get(id: number) {
+    async getByNumber(
+      number: string,
+      program: ProgramCode
+    ): Promise<FetcherResponse<Team | null>> {
+      const response = await api.paginatedFetch<
+        Teams.TeamGetTeams.ResponseBody,
+        Teams.TeamGetTeams.RequestQuery
+      >("/teams", { "number[]": [number], "program[]": [program] });
+
+      if (!response.success) {
+        return response;
+      }
+
+      const team = response.data?.[0];
+      return { success: true, data: team ? new Team(team, api) : null };
+    },
+
+    async get(id: number): Promise<FetcherResponse<Team | null>> {
       const response = await api.fetch<
         Teams.TeamGetTeam.ResponseBody,
         Teams.TeamGetTeam.RequestQuery
       >(`/teams/${id}`);
 
-      return response.success ? new Team(response.data, api) : null;
+      return response.success
+        ? { success: true, data: new Team(response.data, api) }
+        : response;
     },
   };
 }
