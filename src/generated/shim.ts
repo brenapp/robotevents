ddbebd76f8f8b8a17e44a01135c8a8013a7acf60
Brendan McGuire <brendan@bren.app>
2024-08-24 23:03:15 -0500

    Client infrastructure (including pagination)

--- src/generated/shim.ts
+++ src/generated/shim.ts
@@ -1,170 +1,17 @@
 import { paths, components, operations } from "./robotevents.js";
 export { paths, operations };
 
-export type IdInfo<T> = {
-  id: number;
-  name: string;
-  code?: T | null;
-};
-
-/**
- * Programs & Seasons
- **/
-export const programs = {
-  V5RC: 1,
-  VURC: 4,
-  WORKSHOP: 37,
-  VIQRC: 41,
-  NRL: 43,
-  ADC: 44,
-  TVRC: 46,
-  TVIQRC: 47,
-  VRAD: 51,
-  BellAVR: 55,
-  FAC: 56,
-  VAIRC: 57,
-} as const;
-
-export type ProgramAbbr = keyof typeof programs;
-export type ProgramCode = (typeof programs)[ProgramAbbr];
-
-export const years = [
-  "2024-2025",
-  "2023-2024",
-  "2022-2023",
-  "2021-2022",
-  "2020-2021",
-  "2019-2020",
-  "2018-2019",
-  "2017-2018",
-  "2016-2017",
-  "2015-2016",
-  "2014-2015",
-  "2013-2014",
-  "2012-2013",
-  "2011-2012",
-  "2010-2011",
-  "2009-2010",
-] as const;
-
-export type Year = (typeof years)[number];
-
-export const seasons: {
-  [P in ProgramCode]: {
-    [Y in Year]?: number;
-  };
-} = {
-  [programs.V5RC]: {
-    "2024-2025": 190,
-    "2023-2024": 181,
-    "2022-2023": 173,
-    "2021-2022": 154,
-    "2020-2021": 139,
-    "2019-2020": 130,
-    "2018-2019": 125,
-    "2017-2018": 119,
-    "2016-2017": 115,
-    "2015-2016": 110,
-    "2014-2015": 102,
-    "2013-2014": 92,
-    "2012-2013": 85,
-    "2011-2012": 73,
-    "2010-2011": 7,
-    "2009-2010": 1,
-  },
-  [programs.VURC]: {
-    "2024-2025": 191,
-    "2023-2024": 182,
-    "2022-2023": 175,
-    "2021-2022": 156,
-    "2020-2021": 140,
-    "2019-2020": 131,
-    "2018-2019": 126,
-    "2017-2018": 120,
-    "2016-2017": 116,
-    "2015-2016": 111,
-    "2014-2015": 103,
-    "2013-2014": 93,
-    "2012-2013": 88,
-    "2011-2012": 76,
-    "2010-2011": 10,
-    "2009-2010": 4,
-  },
-  [programs.VAIRC]: {
-    "2023-2024": 185,
-    "2021-2022": 171,
-  },
-  [programs.WORKSHOP]: {
-    "2023-2024": 118,
-    "2015-2016": 113,
-    "2014-2015": 107,
-    "2013-2014": 98,
-  },
-  [programs.VIQRC]: {
-    "2024-2025": 189,
-    "2023-2024": 180,
-    "2022-2023": 174,
-    "2021-2022": 155,
-    "2020-2021": 138,
-    "2019-2020": 129,
-    "2018-2019": 124,
-    "2017-2018": 121,
-    "2016-2017": 114,
-    "2015-2016": 109,
-    "2014-2015": 101,
-    "2013-2014": 96,
-  },
-  [programs.NRL]: {
-    "2019-2020": 137,
-  },
-  [programs.ADC]: {
-    "2024-2025": 192,
-    "2023-2024": 184,
-    "2022-2023": 176,
-    "2021-2022": 158,
-    "2020-2021": 144,
-    "2019-2020": 134,
-  },
-  [programs.TVRC]: {
-    "2023-2024": 187,
-    "2022-2023": 179,
-    "2021-2022": 167,
-    "2020-2021": 142,
-    "2019-2020": 136,
-  },
-  [programs.TVIQRC]: {
-    "2023-2024": 186,
-    "2022-2023": 178,
-    "2021-2022": 166,
-    "2020-2021": 141,
-    "2019-2020": 135,
-  },
-  [programs.VRAD]: {},
-  [programs.BellAVR]: {},
-  [programs.FAC]: {
-    "2023-2024": 188,
-    "2022-2023": 177,
-    "2021-2022": 165,
-  },
-};
-
-/**
- * Re-export (and fix) components
- **/
-export type Program = IdInfo<ProgramAbbr>;
-type SpecifyProgramIdInfo<T> = Omit<T, "program"> & {
-  program: Program;
-};
-
-export type Event = SpecifyProgramIdInfo<components["schemas"]["Event"]>;
+export type Event = components["schemas"]["Event"];
 export type EventType = components["schemas"]["EventType"];
+export type Program = components["schemas"]["Program"];
+export type IdInfo = components["schemas"]["IdInfo"];
 export type EventLevel = components["schemas"]["EventLevel"];
 export type Location = components["schemas"]["Location"];
 export type Coordinates = components["schemas"]["Coordinates"];
 export type Locations = components["schemas"]["Locations"];
 export type Division = components["schemas"]["Division"];
 export type Grade = components["schemas"]["Grade"];
-export type Team = SpecifyProgramIdInfo<components["schemas"]["Team"]>;
+export type Team = components["schemas"]["Team"];
 export type MatchObj = components["schemas"]["MatchObj"];
 export type Alliance = components["schemas"]["Alliance"];
 export type AllianceTeam = components["schemas"]["AllianceTeam"];
@@ -179,7 +26,7 @@ export type Error = components["schemas"]["Error"];
 export type PageMeta = components["schemas"]["PageMeta"];
 export type Paginated<T> = {
   meta?: PageMeta;
-  data: T[];
+  data?: T[];
 };
 export type PaginatedTeam = Paginated<Team>;
 export type PaginatedEvent = Paginated<Event>;
@@ -189,8 +36,3 @@ export type PaginatedRanking = Paginated<Ranking>;
 export type PaginatedMatch = Paginated<MatchObj>;
 export type PaginatedSkill = Paginated<Skill>;
 export type PaginatedProgram = Paginated<Program>;
-
-/**
- * Matches
- **/
-export { Round } from "../wrappers/Match.js";
---
+++ src/utils/client.ts
@@ -0,0 +1,186 @@
+import { FilterKeys, PathsWithMethod } from "openapi-typescript-helpers";
+import type { Paginated, paths } from "../generated/shim.js";
+
+import createOpenAPIClient, {
+  type ClientOptions as OpenAPIClientOptions,
+  type Client as OpenAPIClient,
+  FetchResponse,
+  FetchOptions,
+} from "openapi-fetch";
+
+export type PaginatablePaths = {
+  [Path in PathsWithMethod<paths, "get">]: FetchResponse<
+    paths[Path]["get"],
+    unknown,
+    "application/json"
+  >["data"] extends Paginated<unknown> | undefined
+    ? Path
+    : never;
+}[keyof paths];
+
+export type UnwrappedPaginatedPathTypes = {
+  [Path in PathsWithMethod<paths, "get">]: FetchResponse<
+    paths[Path]["get"],
+    unknown,
+    "application/json"
+  >["data"] extends Paginated<infer W> | undefined
+    ? W[]
+    : never;
+};
+
+export type PaginatedPaths = {
+  [Path in PaginatablePaths]: FetchResponse<
+    paths[Path]["get"],
+    unknown,
+    "application/json"
+  >["data"] extends Paginated<infer W> | undefined
+    ? {
+        get: {
+          parameters: paths[Path]["get"]["parameters"];
+          requestBody?: paths[Path]["get"]["requestBody"];
+          responses: Omit<paths[Path]["get"]["responses"], 200> & {
+            200: {
+              headers: {
+                [name: string]: unknown;
+              };
+              content: {
+                "application/json": W[];
+              };
+            };
+          };
+        };
+      }
+    : never;
+};
+
+export type ClientCustomMethods = {
+  PaginatedGET<
+    Path extends PaginatablePaths,
+    Init extends FetchOptions<FilterKeys<paths[Path], "get">>
+  >(
+    path: Path,
+    init: Init
+  ): Promise<
+    FetchResponse<PaginatedPaths[Path]["get"], {}, "application/json">
+  >;
+};
+
+type R = FetchResponse<
+  paths["/events"]["get"],
+  unknown,
+  "application/json"
+>["data"];
+type P = FetchResponse<
+  PaginatedPaths["/events"]["get"],
+  unknown,
+  "application/json"
+>["data"];
+
+export type Client = OpenAPIClient<paths, "application/json"> &
+  ClientCustomMethods;
+
+export type ClientOptions = {
+  authorization: {
+    token: string;
+  };
+  request?: OpenAPIClientOptions;
+};
+
+export function createClient(options: ClientOptions): Client {
+  const base = createOpenAPIClient<paths, "application/json">({
+    baseUrl: "https://www.robotevents.com/api/v2",
+    headers: {
+      Authorization: `Bearer ${options.authorization.token}`,
+      ...options.request?.headers,
+    },
+    ...options.request,
+  });
+
+  const PaginatedGET: ClientCustomMethods["PaginatedGET"] = async <
+    Path extends PaginatablePaths,
+    Init extends FetchOptions<FilterKeys<paths[Path], "get">>
+  >(
+    path: Path,
+    init: Init
+  ) => {
+    const first = await base.GET(path, {
+      ...init,
+      params: { query: { ...init.params?.query, page: 1, per_page: 250 } },
+    });
+
+    if (first.error) {
+      return { response: first.response, error: first.error };
+    }
+
+    const firstData = first.data as Paginated<
+      UnwrappedPaginatedPathTypes[Path]
+    >;
+
+    const data: UnwrappedPaginatedPathTypes[Path] = [];
+
+    if (firstData.data) {
+      data.push(...firstData.data);
+    }
+
+    const last = firstData.meta?.last_page ?? 0;
+    for (let page = 2; page <= last; page++) {
+      const next = await base.GET(path, {
+        ...init,
+        params: { query: { ...init.params?.query, page, per_page: 250 } },
+      });
+
+      if (next.error) {
+        return { response: next.response, error: next.error };
+      }
+
+      const nextData = next.data as Paginated<
+        UnwrappedPaginatedPathTypes[Path]
+      >;
+
+      if (nextData.data) {
+        data.push(...nextData.data);
+      }
+    }
+
+    return {
+      response: new Response(),
+      data: data as NonNullable<
+        FetchResponse<
+          PaginatedPaths[Path]["get"],
+          {},
+          "application/json"
+        >["data"]
+      >,
+    };
+  };
+
+  return { ...base, PaginatedGET };
+}
+
+export async function transformResponse<
+  T,
+  K,
+  M extends `${string}/${string}`,
+  N
+>(
+  response: Promise<FetchResponse<T, K, M>>,
+  transform: (
+    data: NonNullable<FetchResponse<T, K, M>["data"]>,
+    response: FetchResponse<T, K, M>
+  ) => N | Promise<N>[]
+): Promise<FetchResponse<N, K, M>> {
+  const resp = await response;
+
+  if (resp.error) {
+    return { response: resp.response, error: resp.error };
+  }
+
+  if (!resp.data) {
+    return { response: resp.response, data: undefined, error: undefined };
+  }
+
+  const data = (await transform(resp.data, resp)) as NonNullable<
+    FetchResponse<N, K, M>["data"]
+  >;
+  return { response: resp.response, data, error: undefined };
+}
