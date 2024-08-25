f8442a32df33215a28b243812d726b576bd94c36
Brendan McGuire <brendan@bren.app>
2024-08-25 01:29:23 -0500

    Get Team by Number

--- src/endpoints/teams.ts
+++ src/endpoints/teams.ts
@@ -1,4 +1,5 @@
 import { operations } from "../generated/shim.js";
+import { ProgramCode } from "../types.js";
 import { Client, transformResponse } from "../utils/client.js";
 import { Team } from "../wrappers/Team.js";
 
@@ -28,6 +29,27 @@ export function teamsEndpoints(client: Client) {
       return transformResponse(
         client.PaginatedGET("/teams", { params: { query }, ...options }),
         (data) => data.map((team) => new Team(team, client))
+      );
+    },
+
+    /**
+     * Get Team by Number
+     * @param number Team Number
+     * @param program Program Code
+     * @param options Request Options
+     * @returns Team
+     */
+    async getByNumber(
+      number: string,
+      program: ProgramCode,
+      options?: Omit<RequestInit, "body" | "headers">
+    ) {
+      return transformResponse(
+        client.PaginatedGET("/teams", {
+          params: { query: { "number[]": [number], "program[]": [program] } },
+          ...options,
+        }),
+        (data) => (data.length > 0 ? new Team(data[0], client) : null)
       );
     },
   };
